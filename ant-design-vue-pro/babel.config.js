1a8ec5c5ba2802cac3d6aaee950609137695ac88
Sheng14 <1336840941@qq.com>
2020-12-07 09:17:03 +0800

    feat: 如何自定义Webpack和Babel配置

--- ant-design-vue-pro/babel.config.js
+++ ant-design-vue-pro/babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"]
+ // presets: ["@vue/cli-plugin-babel/preset"],
+   presets: [ [ "@vue/app", { useBuiltIns: "entry" } ] ],
+  "plugins": [
+    ["import", { "libraryName": "ant-design-vue", "libraryDirectory": "es", "style": true }] // `style: true` 会加载 less 文件
+  ]
+//  resets: [ [ "@vue/app", { useBuiltIns: "entry" } ] ]
 };
--- ant-design-vue-pro/src/main.js
+++ ant-design-vue-pro/src/main.js
@@ -2,8 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { Button } from "ant-design-vue";
+// import antd from "ant-design-vue";
+// import "ant-design-vue/dist/antd.less";
 
 Vue.config.productionTip = false;
+Vue.use(Button);
+// Vue.use(antd);
 
 new Vue({
   router,
